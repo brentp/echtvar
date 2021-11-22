77935d32fd5710aade3e90822e0e14a097413aa5
Brent Pedersen <bpederse@gmail.com>
2021-11-22 13:39:03 +0100

    save

---
+++ README.md
@@ -0,0 +1,67 @@
+4 bits * 3 billion == 1.5GB
+
+https://immunant.com/blog/2020/01/bitfields/
+
+usage
+=====
+
+make a new afenc file 
+```
+echtvar \
+   encode \
+   # set a multiplier as internally, everything is stored as int
+   --field AF:gnomad_af:1000000 \ 
+   # integers will have a multiplier of 1.
+   --field AC:gnomac_ac \
+   -o gnomad.afenc \ # output file name
+   $input_vcf
+
+```
+
+annotate a VCF with an afen file
+
+```
+echtvar annotate \
+   -o $cohort.afenc.bcf \
+   -a gnomad.afenc \
+   -a ukbiobank.afenc \
+   -f 'gnomad_af < 0.01' \
+   $cohort.bcf
+```
+
+# rust
+
+```rust
+fn annotate(&self, pos:uint64, ref:string, alt:string) -> Result<Vec<int32>, Error> 
+
+extern crate libc;
+
+#[repr(C), align(1)]
+#[derive(BitfieldStruct, Clone, Copy)]
+pub struct Var32 {
+    #[bitfield(name = "enc", ty = "libc::c_uint32", bits = "0..4")]
+    #[bitfield(name = "alen", ty = "libc::c_uint32", bits = "4..8")]
+    #[bitfield(name = "rlen", ty = "libc::c_uint32", bits = "8..12")]
+    #[bitfield(name = "position", ty = "libc::c_uint32", bits = "12..32")]
+    datata: [u8, 4]
+}
+```
+
+type Var64 = struct {
+  
+
+
+
+bits
+====
+
+  01: AF < 1e-5
+  10: AF < 1e-3
+  11: AF < 1e-2
+ 1  : AN < 70%
+
+
+  01: AF < 1e-2
+  10: AF < 1e-3
+  11: AN >k
+
---
+++ src/main.rs
@@ -0,0 +1,93 @@
+extern crate rust_htslib;
+extern crate libc;
+extern crate bitpacking;
+use crate::rust_htslib::bcf::{Reader, Read};
+use crate::rust_htslib::bcf::record::{Record, Buffer};
+use std::borrow::{Borrow, BorrowMut};
+use std::io::Write;
+use bitpacking::{BitPacker8x, BitPacker};
+use c2rust_bitfields::BitfieldStruct;
+
+
+//fn get_float_field<'a, B: BorrowMut<Buffer> + Borrow<Buffer> + 'a>(rec: &Record, field: &[u8], buffer: B) -> f32 {
+fn get_float_field<'a, B: BorrowMut<Buffer> + 'a>(rec: &Record, field: &[u8], buffer: B) -> f32 {
+	return match rec.info_shared_buffer(field, buffer).float().expect("error reading info") {
+		Some(v) => v[0],
+		None => -1.0,
+	};
+}
+fn get_int_field<'a, B: BorrowMut<Buffer> + Borrow<Buffer> + 'a>(rec: &Record, field: &[u8], buffer: B) -> i32 {
+	return match rec.info_shared_buffer(field, buffer).integer().expect("error reading info") {
+		Some(v) => v[0],
+		None => -1,
+	};
+}
+
+#[repr(C, align(1))]
+#[derive(BitfieldStruct, Clone, Copy)]
+pub struct Var32 {
+    /* enc: 4
+     * alen: 2
+     * rlen: 2
+     * position: 20 */
+    #[bitfield(name = "enc", ty = "libc::uint32_t", bits = "0..=4")]
+    #[bitfield(name = "alen", ty = "libc::uint32_t", bits = "4..=8")]
+    #[bitfield(name = "rlen", ty = "libc::uint32_t", bits = "8..=12")]
+    #[bitfield(name = "position", ty = "libc::uint32_t", bits = "12..=32")]
+    data: [u8; 4]
+}
+
+
+
+fn main() {
+    let mut stderr = std::io::stderr();
+
+    if std::env::args().len() < 2 {
+        println!("expecting arguments: <vcf>")
+    }
+	writeln!(stderr, "{}", std::mem::size_of::<Var32>()).expect("error writing to stderr");
+    let args: Vec<String> = std::env::args().collect();
+    let path = &*args[1];
+	writeln!(stderr, "{}", path).expect("error writing to stderr");
+    let mut vcf = Reader::from_path(path).ok().expect("Error opening vcf.");
+	let header = vcf.header().clone();
+	let mut buffer = Buffer::new();
+
+	let mut acs:Vec<u32> = Vec::new();
+    let bitpacker = BitPacker8x::new();
+
+
+	for r in vcf.records() {
+		let mut rec = r.expect("error getting record");
+		let n: &[u8] = header.rid2name(rec.rid().expect("bad rid")).unwrap();
+		let nac: [i32; 1] = [333];
+
+		let af = get_float_field(&rec, b"AF", & mut buffer);
+		//rec.push_info_integer(b"AC", &nac).ok();
+		let ac = get_int_field(&rec, b"AC", & mut buffer);
+        if ac > 3 { continue; }
+		acs.push(ac as u32);
+
+
+        //if acs.len() * std::mem::size_of::<u32>() == 128 {
+        if acs.len() == 256 {
+            let num_bits = bitpacker.num_bits(&acs[..]);
+            let mut compressed = vec![0u8; 4 * BitPacker8x::BLOCK_LEN];
+            let compressed_len = bitpacker.compress(&acs[..], &mut compressed[..], num_bits);
+            writeln!(stderr, "uncompressed bits: {}, compressed bits: {}", acs.len() * std::mem::size_of::<u32>(), compressed_len).expect("error writing to stderr");
+            acs.clear()
+
+        }
+
+		//writeln!(stderr, "{}:{} AF: {} AC: {}", std::str::from_utf8(n).unwrap(), rec.pos(), af, ac).expect("error writing to stderr");
+
+	}
+
+	// https://docs.rs/bitpacking/0.8.4/bitpacking/trait.BitPacker.html
+
+
+	writeln!(stderr, "len: {}", acs.len()).expect("error writing to stderr");
+
+
+
+}
