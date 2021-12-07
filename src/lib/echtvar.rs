7b332a3a107e9b5416c63c0a143cbfea5e804982
Brent Pedersen <bpederse@gmail.com>
2021-12-07 11:07:03 +0100

    fill var32s

--- src/lib/echtvar.rs
+++ src/lib/echtvar.rs
@@ -1,15 +1,12 @@
-use crate::var32;
 use crate::fields;
+use crate::var32;
+use std::fs;
 use std::io;
 use std::io::prelude::*;
-use std::fs;
 
 use byteorder::{LittleEndian, ReadBytesExt};
 
-use stream_vbyte::{
-    decode::decode,
-    x86::Ssse3
-};
+use stream_vbyte::{decode::decode, x86::Ssse3};
 
 #[derive(Clone, Default, Debug, PartialEq, PartialOrd)]
 pub struct EchtVar<T> {
@@ -23,7 +20,7 @@ pub struct EchtVars {
     pub zip: zip::ZipArchive<std::fs::File>,
     pub chrom: String,
     pub start: u32,
-    pub var32s: Vec<var32::Var32>,
+    pub var32s: Vec<u32>,
     pub longs: Vec<var32::LongVariant>,
     pub ints: Vec<EchtVar<u32>>,
     pub floats: Vec<EchtVar<f32>>,
@@ -31,7 +28,6 @@ pub struct EchtVars {
 }
 
 impl EchtVars {
-
     pub fn open(path: &str) -> Self {
         let ep = std::path::Path::new(&*path);
         let file = fs::File::open(ep).expect("error accessing zip file");
@@ -47,47 +43,97 @@ impl EchtVars {
         };
 
         {
-            let mut f = result.zip.by_name("echtvar/config.json").expect("unable to open echtvar/config.json");
+            let mut f = result
+                .zip
+                .by_name("echtvar/config.json")
+                .expect("unable to open echtvar/config.json");
             let mut contents = String::new();
-            f.read_to_string(&mut contents).expect("eror reading config.json");
+            f.read_to_string(&mut contents)
+                .expect("eror reading config.json");
             let flds: Vec<fields::Field> = json5::from_str(&contents).unwrap();
             eprintln!("fields: {:?}", flds);
             for fld in flds {
-                result.ints.push(EchtVar::<u32>{
-                    missing: fld.missing_value as i32, 
+                result.ints.push(EchtVar::<u32> {
+                    missing: fld.missing_value as i32,
                     name: fld.alias,
                     values: vec![],
                 });
-    
             }
         }
         result
     }
 
+    /*
+    pub fn fill(self: &EchtVars, fi: &mut EchtVar<u32>, path: String) -> io::Result<()> {
+        //eprintln!("path:{}", path);
+        let mut iz = self.zip.by_name(&path)?;
+        let n = iz.read_u32::<LittleEndian>()? as usize;
+        //eprintln!("n:{}", n);
+        self.buffer
+            .resize(iz.size() as usize - std::mem::size_of::<u32>(), 0x0);
+        iz.read_exact(&mut self.buffer)?;
+        fi.values.resize(n, 0x0);
+        // TODO: use skip to first position.
+        let bytes_decoded = decode::<Ssse3>(&self.buffer, n, &mut fi.values);
+
+        if bytes_decoded != self.buffer.len() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "didn't read expected number of values from zip",
+            ));
+        }
+        Ok(())
+    }
+    */
+
     #[inline(always)]
     pub fn set_position(self: &mut EchtVars, chromosome: String, position: u32) -> io::Result<()> {
         if chromosome == self.chrom && position >> 20 == self.start >> 20 {
-            return Ok(())
+            return Ok(());
         }
         self.start = position >> 20 << 20; // round to 20 bits.
         self.chrom = chromosome;
         let base_path = format!("echtvar/{}/{}", self.chrom, position >> 20);
         eprintln!("base-path:{}", base_path);
 
-        for fi in &mut self.ints {
+        for fi in self.ints.iter_mut() {
             let path = format!("{}/{}.bin", base_path, fi.name);
-            //eprintln!("path:{}", path);
+            //self.fill(fi, path)?;
             let mut iz = self.zip.by_name(&path)?;
             let n = iz.read_u32::<LittleEndian>()? as usize;
             //eprintln!("n:{}", n);
-            self.buffer.resize(iz.size() as usize - std::mem::size_of::<u32>(), 0x0);
+            self.buffer
+                .resize(iz.size() as usize - std::mem::size_of::<u32>(), 0x0);
             iz.read_exact(&mut self.buffer)?;
             fi.values.resize(n, 0x0);
             // TODO: use skip to first position.
             let bytes_decoded = decode::<Ssse3>(&self.buffer, n, &mut fi.values);
-            
+
             if bytes_decoded != self.buffer.len() {
-                return Err(std::io::Error::new(std::io::ErrorKind::Other, "didn't read expected number of values from zip"))
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "didn't read expected number of values from zip",
+                ));
+            }
+        }
+
+        {
+            let path = format!("{}/var32.bin", base_path);
+            let mut iz = self.zip.by_name(&path)?;
+            let n = iz.read_u32::<LittleEndian>()? as usize;
+            //eprintln!("n:{}", n);
+            self.buffer
+                .resize(iz.size() as usize - std::mem::size_of::<u32>(), 0x0);
+            iz.read_exact(&mut self.buffer)?;
+
+            self.var32s.resize(n, 0x0);
+            let bytes_decoded = decode::<Ssse3>(&self.buffer, n, &mut self.var32s);
+
+            if bytes_decoded != self.buffer.len() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "didn't read expected number of values from zip",
+                ));
             }
         }
 
@@ -98,11 +144,7 @@ impl EchtVars {
         self.longs = serde_json::from_slice(&self.buffer)?;
 
         Ok(())
-
-
     }
-
-
 }
 
 #[cfg(test)]
@@ -115,8 +157,8 @@ mod tests {
 
         assert_eq!(e.ints.len(), 2);
         assert_eq!(e.ints[0].values.len(), 46881);
+        assert_eq!(e.ints[1].values.len(), e.var32s.len());
 
         assert_eq!(e.longs[0].position, 5030185);
-
     }
 }
