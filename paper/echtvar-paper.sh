4097482166ca2bc0093ea3ae9afab7643b83c03c
Brent Pedersen <bpederse@gmail.com>
2022-03-14 14:21:48 +0100

    update script

--- paper/echtvar-paper.sh
+++ paper/echtvar-paper.sh
@@ -1,17 +1,11 @@
-cd /work
-mkdir -p compare
-cd compare
-
 set -exo pipefail
-
 
 qvcf=HG001_GRCh38_1_22_v4.2.1_benchmark.vcf.gz
 qbcf=${qvcf/.vcf.gz/.bcf}
-avcf=/data/gnomad/gnomad.v3.1.2.concat.vcf.gz
-abcf=/data/gnomad/gnomad.v3.1.2.concat.bcf
-fasta=/data/human/GRCh38_full_analysis_set_plus_decoy_hla.fa
+avcf=gnomad.v3.1.2.concat.vcf.gz
+abcf=gnomad.v3.1.2.concat.bcf
+fasta=../human/GRCh38_full_analysis_set_plus_decoy_hla.fa
 
-if [[ ! -f $qvcf ]]; then
 wget -qO - https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/NA12878_HG001/NISTv4.2.1/GRCh38/HG001_GRCh38_1_22_v4.2.1_benchmark.vcf.gz \
   | bcftools norm -m - -f $fasta -O v \
   | awk 'BEGIN{FS=OFS="\t"} ($5 != "*")' \
@@ -20,11 +14,9 @@ wget -qO - https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/NA12878_HG001/NIS
 bcftools index --force $qvcf --threads 3
 bcftools view -O b -o $qbcf --threads 3 $qvcf
 bcftools index --force $qbcf --threads 3
+#DONE
 
-fi
-
-if [[ ! -f gnomad.slivar.v3.1.2.zip ]]; then
-
+<<DONE
 bcftools concat $avcf \
     | slivar make-gnotate --prefix gnomad.slivar.v3.1.2 \
 	-f AC:gnomad_ac \
@@ -39,44 +31,38 @@ bcftools concat $avcf \
 	-f nhomalt_controls_and_biobanks:gnomad_controls_and_biobanks_nhomalt \
       /dev/stdin
 
-fi
+DONE
 
 #<<SLIVAR
 /usr/bin/time -v slivar expr \
     -v $qbcf \
     -g gnomad.slivar.v3.1.2.zip -o slivar-anno.bcf 2> slivar-time.txt
-ssize=$(du -cm gnomad.slivar.v3.1.2.zip | grep total)
-echo "#SIZE: $ssize" >> slivar-time.txt
-
 #SLIVAR
 
 
 
 #<<ECHTVAR
 /usr/bin/time -v \
-  echtvar anno \
-  -e /data/gnomad/gnomad.v3.1.2.echtvar.v2.zip \
+  ~/src/echtvar/target/release/echtvar anno \
+  -e gnomad.v3.1.2.echtvar.v2.zip \
   $qbcf annotated.bcf 2> echtvar-time.txt
-
-esize=$(du -cm /data/gnomad/gnomad.v3.1.2.echtvar.v2.zip | grep total)
-echo "#SIZE: $esize" >> echtvar-time.txt
 
 #ECHTVAR
 
 
 ## VarNote prep
 
-<<DONE
-varnote Index \
+<<VARNOTE
+java -Xmx5G -jar ~/Downloads/VarNote-1.2.0.jar Index \
   -I $avcf -O varnote-index 
-DONE
 
 echo "
 @gnomAD
 fields=[INFO]
-info_fields=[AC, AN, nhomalt, AF, AC_popmax, AN_popmax, nhomalt_popmax, AF_popmax, AF_controls_and_biobanks, nhomalt_controls_and_biobanks]
-out_names=[AC:gnomad_ac, AN:gnomad_an, nhomalt:gnomad_nhomalt, AF:gnomad_af, AC_popmax:gnomad_popmax_ac, AN_popmax:gnomad_popmax_an, nhomalt_popmax:gnomad_popmax_homalt, AF_popmax:gnomad_popmax_af, AF_controls_and_biobanks:gnomad_controls_and_biobanks_af, nhomalt_controls_and_biobanks:gnomad_controls_and_biobanks_nhomalt]
+info_fields=[AC, AF]
+out_names=[AC:gnomAD_AC, AF:gnomAD_AF]
 " > annoc
+VARNOTE
 
 #<<VARNOTE
 
@@ -84,7 +70,7 @@ export TMPDIR=/tmp/
 mkdir -p null/temp # varnote needs this but won't create it.
 
 /usr/bin/time -v \
-varnote Annotation \
+java -Xmx5G -jar ~/Downloads/VarNote-1.2.0.jar Annotation \
   --query-file $qvcf \
   --d-files:db,tag=gnomAD,mode=1 $avcf  \
   --out-format VCF \
@@ -96,8 +82,6 @@ varnote Annotation \
   -A annoc \
   --out-file varnote.anno.vcf.gz 2> varnote-time.txt
 
-vsize=$(du -cm ${avcf}* | grep total)
-echo "#SIZE: $vsize" >> varnote-time.txt
 #VARNOTE
 
 
@@ -106,11 +90,9 @@ echo "#SIZE: $vsize" >> varnote-time.txt
 bcftools annotate \
   -a $abcf \
   --threads 4 \
-  --columns "INFO/gnomad_ac:=INFO/AC,INFO/gnomad_an:=INFO/AN,INFO/gnomad_nhomalt:=INFO/nhomalt,INFO/gnomad_af:=INFO/AF,INFO/gnomad_popmax_ac:=INFO/AC_popmax,INFO/gnomad_popmax_an:=INFO/AN_popmax,INFO/gnomad_popmax_nhomalt:=INFO/nhomalt_popmax,INFO/gnomad_popmax_af:=INFO/AF_popmax,INFO/gnomad_controls_and_biobanks_af:=INFO/AF_controls_and_biobanks,INFO/gnomad_controls_and_biobanks_nhomalt:=INFO/nhomalt_controls_and_biobanks" \
+  --columns "INFO/gnomad_AC:=INFO/AC,INFO/gnomad_AF:=INFO/AF" \
   -O b \
   -o bcft.anno.bcf \
   $qbcf 2> bcftools-time.txt
-bsize=$(du -cm ${bvcf} | grep total)
-echo "#SIZE: $bsize" >> bcftools-time.txt
 
 #BCFTOOLS
